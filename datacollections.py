ca778193d0bccc17ef550087fb141f556e8dabb5
amelas22 <jlemrond@gmail.com>
2024-05-16 18:04:36 -0400

    update for save data to s3

--- datacollections.py
+++ datacollections.py
@@ -1,8 +1,20 @@
 import socketio
 import asyncio
 import aiohttp
+import aiobotocore
+import json
+import os
+from dotenv import load_dotenv
 from sqlalchemy.future import select
 from session import AsyncSessionLocal, DraftSession
+
+load_dotenv()
+
+DO_SPACES_REGION = os.getenv("DO_SPACES_REGION")
+DO_SPACES_ENDPOINT = os.getenv("DO_SPACES_ENDPOINT")
+DO_SPACES_KEY = os.getenv("DO_SPACES_KEY")
+DO_SPACES_SECRET = os.getenv("DO_SPACES_SECRET")
+DO_SPACES_BUCKET = os.getenv("DO_SPACES_BUCKET")
 
 sio = socketio.AsyncClient()
 
@@ -71,5 +83,28 @@ async def save_draft_log_data(session_id, draft_id, draft_data):
                 session.data_received = True  
                 await db_session.commit()
                 print(f"Draft log data saved for {draft_id}")
+                await save_to_digitalocean_spaces(session_id, draft_data)
             else:
                 print(f"Draft session {draft_id} not found in the database")
+
+async def save_to_digitalocean_spaces(session_id, draft_data):
+    aio_session = aiobotocore.get_session()
+    async with aio_session.create_client(
+        's3',
+        region_name=DO_SPACES_REGION,
+        endpoint_url=DO_SPACES_ENDPOINT,
+        aws_access_key_id=DO_SPACES_KEY,
+        aws_secret_access_key=DO_SPACES_SECRET
+    ) as s3_client:
+        try:
+            object_name = f'{session_id}.json'
+            await s3_client.put_object(
+                Bucket=DO_SPACES_BUCKET,
+                Key=object_name,
+                Body=json.dumps(draft_data),
+                ContentType='application/json',
+                ACL='public-read'
+            )
+            print(f"Draft log data uploaded to DigitalOcean Space: {object_name}")
+        except Exception as e:
+            print(f"Error uploading to DigitalOcean Space: {e}")
