cbcbde78366a3c1b9497abf55baf9e89dc1380df
aberdasher <aberdasher.mtg@gmail.com>
2025-03-10 16:28:07 -0400

    refactor for dry and make link more visible

--- sessions/premade_session.py
+++ sessions/premade_session.py
@@ -15,19 +15,11 @@ class PremadeSession(BaseSession):
             "4. After the draft, come back to this message (it'll be in pins) and push Create Rooms and Post Pairings.\n"
             "5. You will now have a private team chat with just your team and a shared draft chat that has pairings and match results. You can select the Match Results buttons to report results.\n"
             "6. Chat channels will automatically close around five hours after the /startdraft command was used."
-            f"\n\n**Chosen Cube: [{session_details.cube_choice}](https://cubecobra.com/cube/list/{session_details.cube_choice})**\n"
-            f"**Draftmancer Session**: **[Join Here]({session_details.draft_link})**"
+            f"{self.get_common_description()}"
         )
         color = discord.Color.blue()
         embed = discord.Embed(title=title, description=description, color=color)
-        
-        cube_choice = session_details.cube_choice
-        if cube_choice == "LSVCube":
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496824956354641/LSVCube.png?ex=67cfad08&is=67ce5b88&hm=16d3350410a3a4c87404c5e6fa1c8ce0408db78a6e841a9133fd69886a1a0db8&")
-        elif cube_choice == "LSVRetro":
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496825417470012/LSVRetro.png?ex=67cfad09&is=67ce5b89&hm=8d4d755e1e47993910f06f886f131b2f7930a8fff022db7651ca3e976d1582ce&")
-        else:
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png")
+        embed.set_thumbnail(url=self.get_thumbnail_url())
         
         embed.add_field(
             name=session_details.team_a_name or "Team A",
--- sessions/random_session.py
+++ sessions/random_session.py
@@ -10,19 +10,11 @@ class RandomSession(BaseSession):
             "2. When enough people join (6 or 8), push Ready Check. Once everyone is ready, push Create Teams.\n"
             "3. Create Teams will create random teams and a corresponding seating order. Draftmancer host needs "
             "to adjust table to match seating order. **TURN OFF RANDOM SEATING IN DRAFTMANCER**\n"
-            f"**Chosen Cube: [{self.session_details.cube_choice}](https://cubecobra.com/cube/list/{self.session_details.cube_choice})**\n"
-            f"**Draftmancer Session**: **[Join Here]({self.session_details.draft_link})**"
+            f"{self.get_common_description()}"
         )
         embed = Embed(title=title, description=description, color=Color.dark_magenta())
         embed.add_field(name="Sign-Ups", value="No players yet.", inline=False)
-        # thumbnail by chosen cube?
-        cube_choice = self.session_details.cube_choice
-        if cube_choice == "LSVCube":
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496824956354641/LSVCube.png?ex=67cfad08&is=67ce5b88&hm=16d3350410a3a4c87404c5e6fa1c8ce0408db78a6e841a9133fd69886a1a0db8&")
-        elif cube_choice == "LSVRetro":
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496825417470012/LSVRetro.png?ex=67cfad09&is=67ce5b89&hm=8d4d755e1e47993910f06f886f131b2f7930a8fff022db7651ca3e976d1582ce&")
-        else:
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png")
+        embed.set_thumbnail(url=self.get_thumbnail_url())
         return embed
 
     def get_session_type(self):
