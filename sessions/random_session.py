4a5827b6a2abd2c95253b3dd5dadcc771c903b8f
Amelas22 <73007083+Amelas22@users.noreply.github.com>
2025-03-10 14:05:23 -0400

    thumbnail updates

--- sessions/random_session.py
+++ sessions/random_session.py
@@ -21,6 +21,8 @@ class RandomSession(BaseSession):
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496824956354641/LSVCube.png?ex=67cfad08&is=67ce5b88&hm=16d3350410a3a4c87404c5e6fa1c8ce0408db78a6e841a9133fd69886a1a0db8&")
         elif cube_choice == "LSVRetro":
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496825417470012/LSVRetro.png?ex=67cfad09&is=67ce5b89&hm=8d4d755e1e47993910f06f886f131b2f7930a8fff022db7651ca3e976d1582ce&")
+        elif cube_choice == "PowerMack":
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1097030242507444226/1348717924978004102/mac.png?ex=67d07af3&is=67cf2973&hm=c750d1ce62a06cc0aa0b224119b4d8a04e3c35e2933cb834f819a8a11061e4f8&")
         else:
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png")
         return embed
