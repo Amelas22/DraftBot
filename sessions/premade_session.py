a07c13c42e83652cc8560989bb349893125b5647
Amelas22 <73007083+Amelas22@users.noreply.github.com>
2025-03-09 23:29:53 -0400

    embed changes

--- sessions/premade_session.py
+++ sessions/premade_session.py
@@ -20,6 +20,15 @@ class PremadeSession(BaseSession):
         )
         color = discord.Color.blue()
         embed = discord.Embed(title=title, description=description, color=color)
+        
+        cube_choice = session_details.cube_choice
+        if cube_choice == "LSVCube":
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496824956354641/LSVCube.png?ex=67cfad08&is=67ce5b88&hm=16d3350410a3a4c87404c5e6fa1c8ce0408db78a6e841a9133fd69886a1a0db8&")
+        elif cube_choice == "LSVRetro":
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496825417470012/LSVRetro.png?ex=67cfad09&is=67ce5b89&hm=8d4d755e1e47993910f06f886f131b2f7930a8fff022db7651ca3e976d1582ce&")
+        else:
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png")
+        
         embed.add_field(
             name=session_details.team_a_name or "Team A",
             value="No players yet.",
--- sessions/random_session.py
+++ sessions/random_session.py
@@ -16,7 +16,13 @@ class RandomSession(BaseSession):
         embed = Embed(title=title, description=description, color=Color.dark_magenta())
         embed.add_field(name="Sign-Ups", value="No players yet.", inline=False)
         # thumbnail by chosen cube?
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png")
+        cube_choice = self.session_details.cube_choice
+        if cube_choice == "LSVCube":
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496824956354641/LSVCube.png?ex=67cfad08&is=67ce5b88&hm=16d3350410a3a4c87404c5e6fa1c8ce0408db78a6e841a9133fd69886a1a0db8&")
+        elif cube_choice == "LSVRetro":
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1239255966818635796/1348496825417470012/LSVRetro.png?ex=67cfad09&is=67ce5b89&hm=8d4d755e1e47993910f06f886f131b2f7930a8fff022db7651ca3e976d1582ce&")
+        else:
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png")
         return embed
 
     def get_session_type(self):
