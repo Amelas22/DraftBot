2a7351936061192d41e18397af8848e21f739ca7
amelas22 <jlemrond@gmail.com>
2025-03-19 16:28:57 -0400

    update to stakedsession display

--- sessions/staked_session.py
+++ sessions/staked_session.py
@@ -8,17 +8,17 @@ logger = logging.getLogger(__name__)
 class StakedSession(RandomSession):
     def __init__(self, session_details):
         super().__init__(session_details)
-        self.min_stake = 10  # Minimum stake allowed
+        self.min_stake = session_details.min_stake  
         
     def create_embed(self):
         """Create an embed message for a staked draft session."""
-        title = f"Dynamic Money Draft! {self.session_details.cube_choice}"
+        title = f"{self.session_details.cube_choice} Dynamic Money Draft! Minimum Bet: {self.session_details.min_stake} tix  "
         description = (
             f"Queue Opened <t:{self.session_details.draft_start_time}:R>\n\n"
             "**Dynamic Money Draft Queue**\n"
             "1. Sign up and enter your max bet amount.\n"
             "2. Teams will be created randomly. Max bets are **NOT** factored in when making teams\n"
-            f"3. Minimum stake: X tix\n\n"
+            f"3. Minimum bet: {self.session_details.min_stake} tix\n\n"
             "**How it works:**\n"
             "• To be written when a methodology is finalized\n"
             "• Basic rules: Ensure both teams can fulfill all 10/20/50 bets on opposing team\n"
